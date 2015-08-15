5fd8158ebdcc659e72e51aa69caf180415ee8da8
wangke <wangke85@gmail.com>
2015-08-15 19:10:14 +0800

    no message

--- DebugInfo/ServiceProvider.php
+++ DebugInfo/ServiceProvider.php
@@ -10,6 +10,7 @@ namespace Xjtuwangke\DebugInfo;
 
 use Barryvdh\Debugbar\LaravelDebugbar;
 use DebugBar\DataCollector\MessagesCollector;
+use Gitonomy\Git\Commit;
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 use SebastianBergmann\Version;
 use Gitonomy\Git\Repository;
@@ -28,11 +29,28 @@ class ServiceProvider extends BaseServiceProvider
                 $codeCollector->addMessage("version:\n" . $version->getVersion() );
             }
             if( class_exists( Repository::class ) ){
-                $git = new \Gitonomy\Git\Repository(base_path());
-                $codeCollector->addMessage("current commit:\n" . $git->getHeadCommit()->getRevision() );
+                $repository = new \Gitonomy\Git\Repository(base_path());
+                $head = $repository->getHeadCommit();
+                $log = $head->getLog(null,null,5);
+                $commits = array();
+                foreach( $log->getCommits() as $commit ){
+                    $commits[] = $this->commitMessageArray( $commit );
+                }
+                $codeCollector->addMessage("current commit:\n" . $head->getRevision() , 'HEAD' );
+                $codeCollector->addMessage($commits , 'git-log');
             }
             $debugbar->addCollector( $codeCollector );
         }
+    }
+
+    protected function commitMessageArray( Commit $commit ){
+        return array(
+            'hash' => $commit->getHash() ,
+            'revision' => $commit->getRevision() ,
+            'message' => $commit->getMessage() ,
+            'committer' => $commit->getCommitterEmail() ,
+            'data' => $commit->getCommitterDate() ,
+        );
     }
 
     public function register(){
